9297ed513596e53149eaaa2e289ccc335b91ef62
Tamilkumaran-hub <56335683+Tamilkumaran-hub@users.noreply.github.com>
2023-06-28 11:57:52 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,5 +1,6 @@
 def call(credentialsId){
-    sh 'mvn clean package sonar:sonar'
+    sh 'export SONAR_TOKEN="squ_45555e3f212a533e34fae7614d671178ab1ab066"'
+    sh 'mvn clean package sonar:sonar' -Dsonar.login=$SONAR_TOKEN
     //withSonarQubeEnv(credentialsId: credentialsId) {
     //     sh 'mvn clean package sonar:sonar'
     //}
