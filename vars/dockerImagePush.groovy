7a61ffbf1e1d28bc8e0c3813de64c48ccd0cf2b3
Tamilkumaran-hub <56335683+Tamilkumaran-hub@users.noreply.github.com>
2023-06-30 21:36:18 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,15 +1,7 @@
-def call(String project, String ImageTag, String hubUser){
-    // withCredentials([usernamePassword(
-    //         credentialsId: "docker",
-    //         usernameVariable: "USER",
-    //         passwordVariable: "PASS"
-    // )]) 
+def call(String project, String ImageTag, String hubUser){ 
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'pass', usernameVariable: 'user')]) {
     sh "docker login -u '$user' -p '$pass'"
     }
-    // {
-    //     sh "docker login -u '$USER' -p '$PASS'"
-    // }
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"   
 }
